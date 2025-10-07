2be6a2bcc7427d080bc1906869368e451f68ddc6
patricklafrance <pa.lafrance@gmail.com>
2025-10-07 15:09:37 -0400

    style(HoneycombInstrumentationPartialClient): add eslint directive to suppress no-explicit-any rule

--- packages/core/src/HoneycombInstrumentationPartialClient.ts
+++ packages/core/src/HoneycombInstrumentationPartialClient.ts
@@ -1,4 +1,5 @@
 // Using "any" instead of "Span" to avoid taking a dependency on "@opentelemetry/api".
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type FetchRequestHookFunction = (span: any, request: Request | RequestInit) => void | true;
 
 export interface HoneycombInstrumentationPartialClient {
