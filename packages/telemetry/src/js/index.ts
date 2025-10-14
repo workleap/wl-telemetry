6983cc7e91696dda64b05036076176386b5bb85e
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2025-10-14 18:08:29 -0400

    fix: Noop clients export from telemetry package (#105)

--- packages/telemetry/src/js/index.ts
+++ packages/telemetry/src/js/index.ts
@@ -1,33 +1,33 @@
-export type {
-    DefineDocumentLoadInstrumentationOptionsFunction,
-    DefineFetchInstrumentationOptionsFunction,
-    DefineUserInteractionInstrumentationOptionsFunction,
-    DefineXmlHttpRequestInstrumentationOptionsFunction,
-    FetchRequestHookFunction,
-    HoneycombInstrumentationClient,
-    HoneycombSdkFactory,
-    HoneycombSdkOptionsTransformer,
+export {
     NoopHoneycombInstrumentationClient,
-    RegisterHoneycombInstrumentationOptions
+    type DefineDocumentLoadInstrumentationOptionsFunction,
+    type DefineFetchInstrumentationOptionsFunction,
+    type DefineUserInteractionInstrumentationOptionsFunction,
+    type DefineXmlHttpRequestInstrumentationOptionsFunction,
+    type FetchRequestHookFunction,
+    type HoneycombInstrumentationClient,
+    type HoneycombSdkFactory,
+    type HoneycombSdkOptionsTransformer,
+    type RegisterHoneycombInstrumentationOptions
 } from "@workleap/honeycomb";
-export type {
-    GetLogRocketSessionUrlListener,
-    LogRocketInstrumentationClient,
-    LogRocketSdkOptionsTransformer,
-    LogRocketWorkleapPlatformIdentification,
-    LogRocketWorkleapPlatformUserTraits,
+export {
     NoopLogRocketInstrumentationClient,
-    RegisterLogRocketInstrumentationOptions
+    type GetLogRocketSessionUrlListener,
+    type LogRocketInstrumentationClient,
+    type LogRocketSdkOptionsTransformer,
+    type LogRocketWorkleapPlatformIdentification,
+    type LogRocketWorkleapPlatformUserTraits,
+    type RegisterLogRocketInstrumentationOptions
 } from "@workleap/logrocket";
-export type {
-    CreateMixpanelTrackingFunctionOptions,
-    InitializeMixpanelOptions,
-    MixpanelClient,
-    MixpanelEventProperties,
-    MixpanelGlobalEventProperties,
-    MixpanelTrackingFunction,
-    MixpanelTrackingFunctionOptions,
-    NoopMixpanelClient
+export {
+    NoopMixpanelClient,
+    type CreateMixpanelTrackingFunctionOptions,
+    type InitializeMixpanelOptions,
+    type MixpanelClient,
+    type MixpanelEventProperties,
+    type MixpanelGlobalEventProperties,
+    type MixpanelTrackingFunction,
+    type MixpanelTrackingFunctionOptions
 } from "@workleap/mixpanel";
 export { initializeTelemetry, type InitializeTelemetryOptions } from "./initializeTelemetry.ts";
 export { TelemetryClient } from "./TelemetryClient.ts";
